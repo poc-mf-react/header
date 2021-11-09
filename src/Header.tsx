7af86610faa7e82241f05bd16f564a8075616043
silvaisabela <eu.silvaisabela@gmail.com>
2021-11-09 09:45:45 -0300

    initial structure

---
+++ src/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <nav>
+      <a href="/">Inicio</a>
+      <a href="/simular">Simular empréstimo</a>
+      <a href="/atwork">Creditas @Work</a>
+      <a href="/ajuda">ajuda</a>
+    </nav>
+  );
+};
+
+export { Header };
