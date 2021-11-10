afd9f6306effaf9c1e98f4cdd0e2e2da9b1c0032
silvaisabela <eu.silvaisabela@gmail.com>
2021-11-10 13:21:11 -0300

    test: modify test component menu

---
+++ src/components/Menu/__tests__/Menu.test.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import { render, screen } from "@testing-library/react";
+import { Menu } from "../Menu";
+
+const content = "content";
+
+describe("<Menu />", () => {
+  it("renders component with content", () => {
+    render(<Menu>{content}</Menu>);
+
+    expect(screen.getByText(content)).toBeInTheDocument();
+  });
+});
