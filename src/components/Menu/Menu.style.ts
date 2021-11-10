03556b10de24cdc4c6d1ebfe103caab20c28d849
silvaisabela <eu.silvaisabela@gmail.com>
2021-11-10 13:20:28 -0300

    feat: modify component menu

---
+++ src/components/Menu/Menu.style.ts
@@ -0,0 +1,24 @@
+import { styled } from "@creditas-ui/system";
+import { selectTheme } from "@creditas-ui/utilities";
+
+const MenuBackground = styled.div`
+  background: ${selectTheme("colors.neutral.0")};
+  display: flex;
+  width: 100%;
+  height: 80px;
+  position: fixed;
+`;
+
+const Nav = styled.nav`
+  width: 100%;
+  display: flex;
+  height: 80px;
+  align-items: center;
+  padding: 0 65px;
+  box-sizing: border-box;
+  left: 50%;
+  transform: translateX(-50%);
+  position: fixed;
+`;
+
+export { MenuBackground, Nav };
---
+++ src/components/Menu/Menu.tsx
@@ -0,0 +1,13 @@
+import { MenuBackground, Nav } from "./Menu.style";
+
+interface MenuBaseProps {
+  children?: React.ReactNode;
+}
+
+const Menu = ({ children, ...props }: MenuBaseProps): JSX.Element => (
+  <MenuBackground>
+    <Nav {...props}>{children}</Nav>
+  </MenuBackground>
+);
+
+export { Menu };
