c2fb52912b6a852ca7cd2ccfd2c4c66febc47e6c
silvaisabela <eu.silvaisabela@gmail.com>
2021-11-10 13:19:00 -0300

    feat: create component menu brand

---
+++ src/components/Menu/MenuBrand/MenuBrand.tsx
@@ -0,0 +1,10 @@
+import { Box } from "@creditas-ui/layout";
+import { Logo } from "../../../assests/icons/Logo";
+
+const MenuBrand = (): JSX.Element => (
+  <Box marginRight="50px">
+    <Logo />
+  </Box>
+);
+
+export { MenuBrand };
