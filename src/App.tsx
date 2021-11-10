9d0f8c94f2972e5a8eb577ff6c003f1d8dbb471a
silvaisabela <eu.silvaisabela@gmail.com>
2021-11-10 13:24:14 -0300

    feat: create file App for Theme provider

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { ThemeProvider } from "@creditas-ui/system";
+import { standard } from "@creditas-ui/themes";
+import { Header } from "./components/Header";
+
+export const App = () => {
+  return (
+    <ThemeProvider theme={standard}>
+      <Header />
+    </ThemeProvider>
+  );
+};
